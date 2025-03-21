cbcbdff42ece530d37e0ea521fd41b6c15ac4940
tetik-mehmet <tetikmehmet930@gmail.com>
2025-03-21 13:30:05 +0300

    Değişiklikler hakkında açıklayıcı bir mesaj

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import workintech from "/workintech.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
+    <div className="pizza-container">
+      <div className="pizza-header">
+        <h1>Teknolojik Yemekler</h1>
+        <div className="pizza-slogan">
+          <h2>KOD AÇIKTIRIR</h2>
+          <h2>PİZZA, DOYURUR</h2>
+        </div>
+        <button
+          className="aciktim-button"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          AÇIKTIM
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
